ac4571e3c55b930a3c0a13bf685f7ae921b82911
Umit Kablan <umit.kablan@webpros.com>
2026-09-11 09:10:48 +0300

    BUGFIX Check proftpd status with chkconfig

--- cloudlinux8to9/actions/services.py
+++ cloudlinux8to9/actions/services.py
@@ -3,6 +3,7 @@
 import os
 
 from pleskdistup import actions as common_actions
+from pleskdistup.common import util
 
 
 class PostStartProftpd(common_actions.StopStartServices):
@@ -10,5 +11,15 @@ class PostStartProftpd(common_actions.StopStartServices):
         super().__init__(["proftpd.socket"], disable_on_prep=False)
 
     def _is_required(self) -> bool:
+        try:
+            out = util.logged_check_call(["/usr/sbin/chkconfig", "ftp_psa", "--list"])
+            for line in out.splitlines():
+                parts = line.split()
+                if len(parts) > 1 and parts[0] == "ftp_psa":
+                    if parts[1] == "on":
+                        return True
+                    return False
+        except Exception:
+            pass
         return os.path.isfile("/etc/xinetd.d/ftp_psa")
 
